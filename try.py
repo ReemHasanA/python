d863671a09b2f8e599a1e93c7e5a3f818f5a89a5
ReemHasanA <reem.abdalla98@gmail.com>
2025-06-02 12:07:02 +0300

    simple class

--- try.py
+++ try.py
@@ -1,22 +1,10 @@
-def scope_test():
-    def do_local():
-        spam = "local spam"
+class MyClass:
+    """A simple example class"""
+    i = 12345
 
-    def do_nonlocal():
-        nonlocal spam
-        spam = "nonlocal spam"
-
-    def do_global():
-        global spam
-        spam = "global spam"
-
-    spam = "test spam"
-    do_local()
-    print("After local assignment:", spam)
-    do_nonlocal()
-    print("After nonlocal assignment:", spam)
-    do_global()
-    print("After global assignment:", spam)
-
-scope_test()
-print("In global scope:", spam)
+    def f(self):
+        return 'hello world'
+    
+MyClass.i = 1000  # Modify the class variable
+print(MyClass.i)  # Output: 1000
+print(MyClass.f(MyClass))  # Output : hello world
