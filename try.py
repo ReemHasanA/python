cecfaee14db1eee81630527740a6f358853e8587
ReemHasanA <reem.abdalla98@gmail.com>
2025-06-02 14:18:48 +0300

    ..

--- try.py
+++ try.py
@@ -8,3 +8,11 @@ class MyClass:
 MyClass.i = 1000  # Modify the class variable
 print(MyClass.i)  # Output: 1000
 print(MyClass.f(MyClass))  # Output : hello world
+print(MyClass.__doc__)  # Output: A simple example class
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+x = Complex(3.0, -4.5)
+x.r, x.i  # Output: (3.0, -4.5)
